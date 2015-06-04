a1be25f9318cae4bb1346e2defc0ef370e763d7a
steve rhyner <sr@steverhyner.biz>
2015-06-04 15:59:35 -0700

    Update pdoConnect.php

--- pdoConnect.php
+++ pdoConnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$db=new PDO('mysql:host=localhost;dbname=steve_php;charset=utf8','steve_steve','H0ganisnumber!');
+$db=new PDO('mysql:host=localhost;dbname=##;charset=utf8','###','###');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(Exception $e){
 	echo"Caught exception:" .$e->getMessage()."<br>";
